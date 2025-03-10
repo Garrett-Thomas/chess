a324a6a961f5064b078008c15f4f931d0da13b59
Garrett Thomas <garrett@Garretts-MacBook-Pro-4.local>
2025-03-10 14:14:59 -0600

    Created a test

--- server/src/test/java/dao/SqlGameTests.java
+++ server/src/test/java/dao/SqlGameTests.java
@@ -1,9 +1,24 @@
 package dao;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class SqlGameTests {
+   private static final SQLGameDAO SQL_GAME_DAO = new SQLGameDAO();
 
 
+
+    @BeforeAll
+    public static void init(){
+        SQL_GAME_DAO.clear();
+       SQL_GAME_DAO.createGame("test");
+
+    }
+
+    @Test
+    public void testList(){
+        var res = SQL_GAME_DAO.getGames();
+        assert(!res.isEmpty());
+    }
 
 }
