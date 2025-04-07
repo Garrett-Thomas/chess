fb0e90d7e4b9a7692cc9aee0b77aefbb7dfe991b
Garrett Thomas <garrett@Garretts-MacBook-Pro-4.local>
2025-04-07 15:53:43 -0600

    Moving along

--- server/src/main/java/dataaccess/ConnectionManager.java
+++ server/src/main/java/dataaccess/ConnectionManager.java
@@ -1,4 +1,31 @@
 package dataaccess;
 
+import org.eclipse.jetty.websocket.api.Session;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ConnectionManager {
+
+    private static final HashMap<String, Session> connections = new HashMap<>();
+
+    public void broadcast(String excludeName, String msg) throws IOException {
+        ArrayList<String> removeList = new ArrayList<>();
+        for (String name : connections.keySet()) {
+            if (connections.get(name).isOpen()) {
+
+                if (!name.equals(excludeName)) {
+                    connections.get(name).getRemote().sendString(msg);
+                }
+
+            } else {
+                removeList.add(name);
+            }
+        }
+
+        for (String toRemove : removeList) {
+            connections.remove(toRemove);
+        }
+    }
 }
--- server/src/main/java/server/WebSocketHandler.java
+++ server/src/main/java/server/WebSocketHandler.java
@@ -1,6 +1,7 @@
 package server;
 
 import com.google.gson.Gson;
+import dao.SQLAuthDAO;
 import dao.SQLGameDAO;
 import dao.SQLUserDAO;
 import org.eclipse.jetty.websocket.api.*;
@@ -10,30 +11,36 @@ import websocket.commands.UserGameCommand;
 import java.io.IOException;
 import java.util.Timer;
 
-    @WebSocket
-    public class WebSocketHandler {
-        private static SQLUserDAO userDAO = SQLUserDAO.getInstance();
-        private static SQLGameDAO gameDAO = SQLGameDAO.getInstance();
+@WebSocket
+public class WebSocketHandler {
+    private static SQLUserDAO userDAO = SQLUserDAO.getInstance();
+    private static SQLGameDAO gameDAO = SQLGameDAO.getInstance();
+    private static SQLAuthDAO authDAO = SQLAuthDAO.getInstance();
 
-        @OnWebSocketMessage
-        public void onMessage(Session user, String message) {
-            UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
-            switch(command.getCommandType()){
+    @OnWebSocketMessage
+    public void onMessage(Session user, String message) {
+        UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
+        switch (command.getCommandType()) {
 
-                // Implied that the client has already joined a game via http so just need to find user using token
-                case CONNECT -> {
+            // Implied that the client has already joined a game via http so just need to find user using token
+            case CONNECT -> {
+                String username = authDAO.getUsername(command.getAuthToken());
+                var userData = userDAO.getUser(username);
 
 
-                    user.getRemot
-                }
+
+
+
 
             }
 
-
         }
 
-        @OnWebSocketError
-        public void onError(Session session, Throwable error) {
-            System.out.printf("%s %s", session.getProtocolVersion(), error.toString());
-        }
+
     }
+
+    @OnWebSocketError
+    public void onError(Session session, Throwable error) {
+        System.out.printf("%s %s", session.getProtocolVersion(), error.toString());
+    }
+}
